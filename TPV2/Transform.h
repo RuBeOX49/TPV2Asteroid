c3b2df2c53eca16cc549b03f6397365b28b8d066
RuBeOX49 <daniel.alonsoherranz@gmail.com>
2023-02-14 10:50:08 +0100

    Update Transform.h

--- TPV2/Transform.h
+++ TPV2/Transform.h
@@ -2,12 +2,15 @@
 #include "Component.h"
 #include "src/utils/Vector2D.h"
 #include "ecs.h"
+#include <SDL.h>
 class Transform : public Component {
 private:
 
 	Vector2D position;
 	Vector2D velocity;
 
+	int width, height;
+	
 	float rotation;
 
 public:
@@ -22,6 +25,22 @@ public:
 
 	inline Vector2D& getVel() { return velocity; }
 	inline void setVel(Vector2D newVel) { velocity = newVel; }
+
+	inline int getWidth() { return width; }
+	inline void setWidth(int width) { this->width = width; }
+
+	inline int getHeight() { return height; }
+	inline void setHeight(int height) { this->height = height; }
+
+	inline SDL_Rect getRect() {
+		SDL_Rect rect;
+		rect.x = position.getX();
+		rect.y = position.getY();
+		rect.w = width;
+		rect.h = height;
+
+		return rect;
+	}
 
 	inline float getRotation() { return rotation; }
 	inline void setRotation(float newRotation) { rotation = newRotation; }
