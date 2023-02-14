bcf46cbb19dc5e71e1395b899b840264f2d11418
dlbcb <diegol08@ucm.es>
2023-02-14 10:48:54 +0100

    MmmmmMMM

---
+++ TPV2/Image.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Component.h"
+#include "../TPV2/src/sdlutils/Texture.h"
+#include "GameObject.h"
+#include "Transform.h"
+class Image : public Component
+{
+private:
+	Transform* transform;
+	Texture* texture;
+
+public:
+	static const int id = _IMAGE;
+	Image(Texture* texture) :texture(texture), transform(nullptr) {};
+	void initComponent() override;
+	void render() override;
+};
+
