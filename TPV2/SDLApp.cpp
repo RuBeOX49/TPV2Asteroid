eaf023d5a41bd3d7e99a31576e5330c36947942e
RuBeOX49 <daniel.alonsoherranz@gmail.com>
2023-02-15 10:33:25 +0100

    Constructora de SDLApp

--- TPV2/SDLApp.cpp
+++ TPV2/SDLApp.cpp
@@ -1,1 +1,19 @@
 #include "SDLApp.h"
+#include <string>
+
+SDLApp::SDLApp()
+{
+	SDL_Init(SDL_INIT_EVERYTHING);
+	window = SDL_CreateWindow("Practica 2", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+		WIN_WIDTH, WIN_HEIGHT, SDL_WINDOW_SHOWN);
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	if (window == nullptr || renderer == nullptr) throw std::string("Error loading SDL window or renderer");
+
+	//cargado de texturas
+
+	states = new GameStateMachine();
+
+	
+
+
+}
--- TPV2/SDLApp.h
+++ TPV2/SDLApp.h
@@ -1,6 +1,10 @@
 #pragma once
 #include "SDL.h"
 #include "SDL_image.h"
+#include "GameStateMachine.h"
+
+#define WIN_WIDTH 600
+#define WIN_HEIGHT 800
 
 class SDLApp
 {
@@ -8,6 +12,8 @@ private:
 
 	SDL_Renderer* renderer{ nullptr };
 	SDL_Window* window{ nullptr };
+
+	GameStateMachine* states;
 
 public:
 
