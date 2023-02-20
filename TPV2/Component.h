453c968eb43ebaad13c6d89985bc0728bab1d705
RuBeOX49 <daniel.alonsoherranz@gmail.com>
2023-02-20 17:31:43 +0100

    Comoponenentes

--- TPV2/Component.h
+++ TPV2/Component.h
@@ -1,26 +1,33 @@
 #pragma once
+#ifndef COMPONENT_H_
+#define COMPONENT_H_
+#include <SDL.h>
 #include "ecs.h"
-
 class GameObject;
-class Manager;
-class Component
-{
+class GameState;
+
+class Component {
 protected:
-	GameObject* gameObject;
-	Manager* manager;
-
+	GameObject* gObj;
+	GameState* gStt;
 public:
-	Component() :gameObject(nullptr), manager(nullptr) {};
-	virtual ~Component();
-
-	inline void setContext(GameObject* gObject, Manager* mngr) {
-		gameObject = gObject;
-		manager = mngr;
+	// Constructora
+	Component() : gObj(nullptr), gStt(nullptr) {}
+	// Destructora
+	virtual ~Component() {}
+	// Asigna el GameObject y el Manager del Component
+	inline void setContext(GameObject* _gObj, GameState* _gStt) {
+		gObj = _gObj;
+		gStt = _gStt;
 	}
-
-	virtual void initComponent();
-	virtual void render();
-	virtual void update();
-	virtual void handleEvents();
+	// Inicializa el Component si es necesario
+	virtual void initComponent() {}
+	// Actualiza el Component
+	virtual void update() {}
+	// Dibuja el Component en pantalla
+	virtual void render() const {}
+	// Maneja los eventos del Component
+	virtual void handleInput() {}
 };
 
+#endif // !COMPONENT_H_
