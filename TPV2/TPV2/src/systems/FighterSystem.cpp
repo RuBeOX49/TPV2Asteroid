80e7be8c50b073b7079a3352220985c563705597
DDiegoP <95857593+DDiegoP@users.noreply.github.com>
2023-03-31 19:15:13 +0200

    La giracion mal

--- TPV2/TPV2/src/systems/FighterSystem.cpp
+++ TPV2/TPV2/src/systems/FighterSystem.cpp
@@ -1,39 +1,51 @@
 #include "FighterSystem.h"
 
-void FighterSystem::receive(const Message& m)
-{
-}
 
 void FighterSystem::initSystem()
 {
+
+}
+
+void FighterSystem::receive(const Message& m)
+{
+	switch (m.id)
+	{
+	case _m_BATTLE_STATE_SETUP:
+		onRoundStart();
+		active_ = true;
+
+	default:
+		break;
+	}
 }
 
 void FighterSystem::update()
 {
 	if (!active_)
 		return;
-
-
+	fighterTransform->setPos(fighterTransform->getPos() + (fighterTransform->getVel() * Game::instance()->getDeltaTimeSeconds()));
 }
 
 void FighterSystem::handleInput()
 {
 
 
-	/*Vector2D vel = Vector2D();
+	Vector2D vel = Vector2D();
 	if (InputHandler::instance()->isKeyDown(SDLK_a)) {
-		transform->rotate(-rotationFactor);
+		fighterTransform->setRotation(fighterTransform->getRotation()-fighterCtrlData->getRotationFactor());
+		fighterTransform->setVel(fighterTransform->getVel().rotate(fighterTransform->getRotation()));
 	}
 	if (InputHandler::instance()->isKeyDown(SDLK_d)) {
-		transform->rotate(rotationFactor);
+		fighterTransform->setRotation(fighterTransform->getRotation() + fighterCtrlData->getRotationFactor());
+		fighterTransform->setVel(fighterTransform->getVel().rotate(fighterTransform->getRotation()));
 	}
 	if (InputHandler::instance()->isKeyDown(SDLK_w)) {
-		thrust->play();
+		fighterCtrlData->playThrust();
 		vel = vel + Vector2D(0, -1);
-		vel = vel.rotate(transform->getRotation()) * shipSpeed;
-		if (vel.magnitude() != 0 && transform->getVel().magnitude() < 50)
-			transform->setVel(vel + transform->getVel());
-	}*/
+		vel = vel.rotate(fighterTransform->getRotation()) * fighterCtrlData->getVel();
+		if (vel.magnitude() != 0 && fighterTransform->getVel().magnitude() < 50)
+			fighterTransform->setVel(vel + fighterTransform->getVel());
+	}
 
 }
 
@@ -42,9 +54,15 @@ void FighterSystem::onCollision_FighterAsteroid()
 }
 
 void FighterSystem::onRoundOver()
-{
-}
+{}
 
 void FighterSystem::onRoundStart()
 {
+	for (auto var : mngr_->getEntities()) {
+		if (var->getGroup() == _grp_FIGHTER)
+			fighter = var;
+	}
+	fighterCtrlData = mngr_->getComponent<FighterCtrl>(fighter);
+	fighterTransform = mngr_->getComponent<Transform>(fighter);
+
 }
