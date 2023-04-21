0c59fc9d3533e7f3dbe4442bc55e0003fded0b56
RuBeOX49 <daniel.alonsoherranz@gmail.com>
2023-04-21 19:33:53 +0200

    puede que cliente bien??

--- TPV2/TPV2/src/systems/NetSystem.cpp
+++ TPV2/TPV2/src/systems/NetSystem.cpp
@@ -86,50 +86,37 @@ bool NetSystem::client()
 	cout << "Server Host Name: ";
 	cin >> hostName;
 
-	socket = SDLNet_UDP_Open(puerto);
-	if (!socket) {
-		cout << "Error en socket\n";
-		return false;
-	}
-
 	if (SDLNet_ResolveHost(&otherPlayerAdress, hostName.c_str(), puerto) < 0) {
 		std::cout << "Could not resolve hostname";
 		return false;
 	}
 
-
-
-	packet = SDLNet_AllocPacket(MAX_SIZE);
-	if (!packet) {
-		cout << "Error en packet\n";
-		return false;
-	}
-
-	/*
-	
-	socket = SDLNet_UDP_Open(puerto);
+	socket = SDLNet_UDP_Open(0);
 	if (!socket) {
 		cout << "Error en socket\n";
 		return false;
-	} 
+	}
 
 	socketSet = SDLNet_AllocSocketSet(1);
 	if (!socketSet) {
 		cout << "Error en socketset\n";
 		return false;
-	} 
 
-	SDLNet_UDP_AddSocket(socketSet, socket);
+
 	packet = SDLNet_AllocPacket(MAX_SIZE);
 	if (!packet) {
 		cout << "Error en packet\n";
 		return false;
 	}
 
-	*/
-
+	port = SDLNetUtils::getSocketPort(socket);
+	if (!port)
+	{
+		cout << "Error en socket port (cliente)\n";
+		return false;
+	}
 
 	std::cout << "all good so far!\n";
 
-	return false;
+	return true;
 }
